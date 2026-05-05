1bf0848ec572cfeafabef99cf5bb543312e432fb
James Wyckoff <jmswykoff@gmail.com>
2026-05-05 08:20:14 -0500

    Change image rotation interval and add reset feature
    Updated image rotation interval from 3 seconds to 2 seconds 
    Added tool checklist reset functionality

--- script.js
+++ script.js
@@ -1,27 +1,20 @@
-//Interval function
-
-// Timer
-// Start at image 1
+// Image settings
 let currentImage = 1;
-
 let maxImages = 15;
 
-// Start the timer AFTER the page loads
+// Start rotation AFTER the page loads
 window.onload = function () {
     timeChange();
 };
 
 //Timer function to change the image every second
 function timeChange() {
-     setInterval(changeImage, 3000); //change every 3000 milliseconds (3 seconds)
+     setInterval(changeImage, 2000); //change every 2 seconds
 } 
-
 // Image function
 function changeImage() {
-//increment the image number by 1
-  
-    currentImage ++; 
-//if the image number exceeds the total number of images, reset to the first image
+    currentImage ++;
+    
     if (currentImage > maxImages) { 
         currentImage = 1;
     }
@@ -29,8 +22,11 @@ function changeImage() {
 //change the image source to the new image
     document.getElementById("container").src = currentImage + ".JPG"; 
 }
-timeChange();
 
 
-
-document.querySelectorAll(".tool-checklist input[type='checkbox']")
+// Tool checklist reset
+document.getElementById("reset-tools").addEventListener("click", () => {
+                    document.querySelectorAll(".tool-checklist input[type='checkbox']").forEach(box => {
+                        box.checked = false;
+                    });
+                });
